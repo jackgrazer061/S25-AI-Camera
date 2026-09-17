4e1e66e0ff5b80d48437c97e58ca8fb243494ef5
jackgrazer061 <jackgrazer061@gmail.com>
2026-09-17 17:41:16 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,10 @@ android {
         versionCode = 2
         versionName = "0.2"
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     buildTypes { release { isMinifyEnabled = false } }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
